a622de1c909f2b813b679b19fc6b27996da72289
Anthony Perez <anthony@amazingant.com>
2016-08-22 18:55:12 +0000

    Automated version bump to 0.1.0

--- Source/Amazingant.FSharp.TypeExpansion/AssemblyInfo.fs
+++ Source/Amazingant.FSharp.TypeExpansion/AssemblyInfo.fs
@@ -1,4 +1,15 @@
 namespace System
 open System.Reflection
 
+[<assembly: AssemblyTitleAttribute("Amazingant.FSharp.TypeExpansion")>]
+[<assembly: AssemblyProductAttribute("Amazingant.FSharp.TypeExpansion")>]
+[<assembly: AssemblyDescriptionAttribute("Code generation tool that applies expansion functions to simple types")>]
+[<assembly: AssemblyCopyrightAttribute("Copyright © 2016 amazingant (Anthony Perez)")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
 do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
+    let [<Literal>] InformationalVersion = "0.1.0"
