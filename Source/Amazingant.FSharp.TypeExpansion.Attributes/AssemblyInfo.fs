ac595591b2e27251f4aa9ab6b16ec0a0ee8a517a
Anthony Perez <anthony@amazingant.com>
2016-08-26 13:52:43 +0000

    Update AssemblyInfo for attributes project

--- Source/Amazingant.FSharp.TypeExpansion.Attributes/AssemblyInfo.fs
+++ Source/Amazingant.FSharp.TypeExpansion.Attributes/AssemblyInfo.fs
@@ -1,41 +1,15 @@
-﻿namespace Amazingant.FSharp.TypeExpansion.Attributes.AssemblyInfo
-
+namespace System
 open System.Reflection
-open System.Runtime.CompilerServices
-open System.Runtime.InteropServices
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[<assembly: AssemblyTitle("Amazingant.FSharp.TypeExpansion.Attributes")>]
-[<assembly: AssemblyDescription("")>]
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("Amazingant.FSharp.TypeExpansion.Attributes")>]
-[<assembly: AssemblyCopyright("Copyright ©  2016")>]
-[<assembly: AssemblyTrademark("")>]
-[<assembly: AssemblyCulture("")>]
+[<assembly: AssemblyTitleAttribute("Amazingant.FSharp.TypeExpansion.Attributes")>]
+[<assembly: AssemblyProductAttribute("Amazingant.FSharp.TypeExpansion.Attributes")>]
+[<assembly: AssemblyDescriptionAttribute("Code generation tool that applies expansion functions to simple types")>]
+[<assembly: AssemblyCopyrightAttribute("Copyright © 2016 amazingant (Anthony Perez)")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+do ()
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[<assembly: ComVisible(false)>]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("bc174d36-42ea-46dd-b7c5-2306317e75ff")>]
-
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
-
-do
-    ()
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.1.0"
+    let [<Literal>] InformationalVersion = "0.1.0"
