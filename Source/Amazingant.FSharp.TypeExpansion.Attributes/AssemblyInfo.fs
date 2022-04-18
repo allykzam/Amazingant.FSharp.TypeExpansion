1b50ac81abde93203086a2672b8ff7ea1de77ebc
Anthony Perez <anthony@amazingant.com>
2022-04-18 11:43:10 -0400

    Automated version bump to 2.3.0

--- Source/Amazingant.FSharp.TypeExpansion.Attributes/AssemblyInfo.fs
+++ Source/Amazingant.FSharp.TypeExpansion.Attributes/AssemblyInfo.fs
@@ -5,11 +5,11 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Amazingant.FSharp.TypeExpansion.Attributes")>]
 [<assembly: AssemblyDescriptionAttribute("Code generation tool that applies expansion functions to simple types")>]
 [<assembly: AssemblyCopyrightAttribute("Copyright © 2016-2022 amazingant (Anthony Perez)")>]
-[<assembly: AssemblyVersionAttribute("2.2.1")>]
-[<assembly: AssemblyFileVersionAttribute("2.2.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("2.2.1")>]
+[<assembly: AssemblyVersionAttribute("2.3.0")>]
+[<assembly: AssemblyFileVersionAttribute("2.3.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("2.3.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "2.2.1"
-    let [<Literal>] InformationalVersion = "2.2.1"
+    let [<Literal>] Version = "2.3.0"
+    let [<Literal>] InformationalVersion = "2.3.0"
